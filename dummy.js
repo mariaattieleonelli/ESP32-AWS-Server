2a4788d2418f38bf28600f561e3030e9df905f0c
María <36321142+mariaattieleonelli@users.noreply.github.com>
2021-03-18 19:32:39 -0600

    Add files via upload

---
+++ dummy.js
@@ -0,0 +1,25 @@
+//const numeros = [10, 4, 2, 100, 4];
+const usuarios = [
+    {
+        id: 0,
+        nombre: 'María',
+        clave: 'aaammmmffffc'
+    },
+    {
+        id: 20,
+        nombre: 'Churu',
+        clave: 'zpfg'
+    },
+    {
+        id: 2,
+        nombre: 'Kike',
+        clave: 'lllld'
+    },
+    {
+        id: 1, 
+        nombre: 'Eren',
+        clave: 'abc'
+    },
+];
+
+module.exports = usuarios;
---
+++ main.js
@@ -0,0 +1,7 @@
+const app = require('./app');
+
+const port = 8000;
+
+app.listen(port, ()=>{
+    console.log("Servidor corriendo correctamente en el puerto 8000");
+});
